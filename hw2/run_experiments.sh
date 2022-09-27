604a57371db611a4adddaf8c081190973263d93e
Terence Neo <terencenyy@yahoo.com.sg>
2022-09-26 21:17:41 -0700

    Update run_experiments.sh

--- hw2/run_experiments.sh
+++ hw2/run_experiments.sh
@@ -76,15 +76,23 @@
 # --reward_to_go --nn_baseline --action_noise_std 0.5 --gae_lambda 1 \
 # --exp_name q5_b2000_r0.001_lambda1
 
+# Experiment 3
+echo running experiment 3...
+
+python cs285/scripts/run_hw2.py \
+--env_name LunarLanderContinuous-v2 --ep_len 1000 \
+--discount 0.99 -n 100 -l 2 -s 64 -b 40000 -lr 0.005 \
+--reward_to_go --nn_baseline --exp_name q3_b40000_r0.005
+
 # HalfCheetah with optimal values -b 50000 -lr 0.02
 echo running experiment 4 HalfCheetah with optimal values -b 50000 -lr 0.02...
 # python cs285/scripts/run_hw2.py --env_name HalfCheetah-v4 --ep_len 150 \
 # --discount 0.95 -n 100 -l 2 -s 32 -b 50000 -lr 0.02 \
 # --exp_name q4_b50000_r0.02
 
-python cs285/scripts/run_hw2.py --env_name HalfCheetah-v4 --ep_len 150 \
---discount 0.95 -n 100 -l 2 -s 32 -b 50000 -lr 0.02 -rtg \
---exp_name q4_b50000_r0.02_rtg
+# python cs285/scripts/run_hw2.py --env_name HalfCheetah-v4 --ep_len 150 \
+# --discount 0.95 -n 100 -l 2 -s 32 -b 50000 -lr 0.02 -rtg \
+# --exp_name q4_b50000_r0.02_rtg
 
 python cs285/scripts/run_hw2.py --env_name HalfCheetah-v4 --ep_len 150 \
 --discount 0.95 -n 100 -l 2 -s 32 -b 50000 -lr 0.02 --nn_baseline \
@@ -93,14 +101,6 @@ python cs285/scripts/run_hw2.py --env_name HalfCheetah-v4 --ep_len 150 \
 python cs285/scripts/run_hw2.py --env_name HalfCheetah-v4 --ep_len 150 \
 --discount 0.95 -n 100 -l 2 -s 32 -b 50000 -lr 0.02 -rtg --nn_baseline \
 --exp_name q4_b50000_r0.02_rtg_nnbaseline
-
-# Experiment 3
-echo running experiment 3...
-
-python cs285/scripts/run_hw2.py \
---env_name LunarLanderContinuous-v2 --ep_len 1000 \
---discount 0.99 -n 100 -l 2 -s 64 -b 40000 -lr 0.005 \
---reward_to_go --nn_baseline --exp_name q3_b40000_r0.005
 
 # Experiment 4 continue
 echo running experiment 4 continue...
